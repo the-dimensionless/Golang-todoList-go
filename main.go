95871bf0f171c7cdc0b07450277e11b761cf548b
Patnam Madhu <patnam.madhu@brillio.com>
2022-06-06 11:11:29 +0530

    initial code setup

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main(){
+import (
 
+	"time"
+	"github.com/thedevsaddam/renderer"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var rnd *renderer.Render
+var db *mgo.Database
+
+const (
+	hostName       string = "localhost:27017"
+	dbName         string = "demo_todo"
+	collectionName string = "todo"
+	port           string = ":9000"
+)
+
+type (
+	todoModel struct {
+		ID        bson.ObjectId `bson:"_id,omitempty"`
+		Title     string        `bson:"title"`
+		Completed bool          `bson:"completed"`
+		CreatedAt time.Time     `bson:"createAt"`
+	}
+
+	todo struct {
+		ID        string    `json:"id"`
+		Title     string    `json:"title"`
+		Completed bool      `json:"completed"`
+		CreatedAt time.Time `json:"created_at"`
+	}
+)
+
+func main(){
+	
 }
